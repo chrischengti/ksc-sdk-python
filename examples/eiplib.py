dcea93c15841f98f13ed2d6a165b4e685d21a7f2
ziguang <chenziguang@hotmail.com>
2018-06-15 22:17:17 +0800

    add funcs

--- examples/eiplib.py
+++ examples/eiplib.py
@@ -11,8 +11,8 @@ import sys
 import time
 from kscore.exceptions import ClientError
 
-ks_access_key_id ='AKLT3JPVchN4SJ2xLSTV_fIDqQ'
-ks_secret_access_key = 'OCEADKOcWtbtygk3rB68NL1UjBrDxxJygvjPSCr/QXPuheeA4UlIAYxDJ9XNdYoUwQ=='
+ks_access_key_id =''
+ks_secret_access_key = ''
 region = 'cn-shanghai-2'
 
 
@@ -24,13 +24,20 @@ def getLines():
     else:
         print res 
         return res['LineSet']
-def describeAddresses():
+def describeAddresses(AllocationIdN=None,ProjectIdN=None,MaxResults=None,NextToken=None):
+    param = {}
+    if AllocationIdN:
+        param.update(dict(("AllocationId.{}".format(index), item) for index, item in enumerate(AllocationIdN, 1)))
+    if ProjectIdN:
+        param.update(dict(("ProjectId.{}".format(index), item) for index, item in enumerate(ProjectIdN, 1)))        
     try:
-        res = eipClient.describe_addresses()
+        if param:
+            print param
+            res = eipClient.describe_addresses(**param)
     except ClientError, e:
         print 'error '+str(e)
     else:
-	for eip in res['AddressesSet']:
+        for eip in res['AddressesSet']:
             print eip
 def allocateAddress(LineId,BandWidth,ChargeType,PurchaseTime=0,ProjectId=0):
     res = None
@@ -76,20 +83,31 @@ def releaseAddress(AllocationId):
         print 'error '+str(e)
     else:
         print res
-
+def GetAccountAllProjectList():
+    try:
+        res = iamClient.get_account_all_project_list()
+    except ClientError, e:
+        print 'error '+str(e)
+    else:
+        return res['ListProjectResult']['ProjectList']
 if __name__ == "__main__":
     s = get_session()
-    s.set_credentials(ks_access_key_id, ks_secret_access_key)
+    #s.set_credentials(ks_access_key_id, ks_secret_access_key)
     vifClient = s.create_client("vpc", region, use_ssl=True)
     kecClient = s.create_client("kec", region, use_ssl=True)
     eipClient = s.create_client("eip", region, use_ssl=True)
     slbClient = s.create_client("slb", region, use_ssl=True)
+    iamClient = s.create_client("iam", region, use_ssl=True)
     lineId = None
     for line in getLines():
         if line['LineName'] == 'BGP':
             lineId = line['LineId']
     #allocateAddress(lineId,1,'Daily')u
-    describeAddresses()
+    eips=['d2a6c424-8d32-424f-84d5-f41aef59fd11','ecfae0a6-8d8c-49c9-aa04-20bc2492fc3a']
+    projects=[1]
+    #describeAddresses(ProjectIdN=projects)
+    describeAddresses(AllocationIdN=eips,ProjectIdN=projects)
+    print GetAccountAllProjectList()
     #associateAddress('2835602d-d12d-4799-bfb0-0331ef1e4edc','Ipfwd','8749bb48-0c63-444a-80ce-0bd9d0d91f34','8fced74a-e232-4916-8023-1febd55fb1a2')
     #disassociateAddress('2835602d-d12d-4799-bfb0-0331ef1e4edc')
     #releaseAddress('2835602d-d12d-4799-bfb0-0331ef1e4edc')

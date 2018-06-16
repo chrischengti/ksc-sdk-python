321c540dda83fe12dfed9d8ded465d3fbb71948d
ziguang <chenziguang@hotmail.com>
2018-06-16 08:11:34 +0800

    update

--- examples/eiplib.py
+++ examples/eiplib.py
@@ -7,7 +7,7 @@ examples
 '''
 
 from kscore.session import get_session
-import sys
+import sys,pprint
 import time
 from kscore.exceptions import ClientError
 
@@ -19,11 +19,10 @@ region = 'cn-shanghai-2'
 def getLines():
     try:
         res = eipClient.get_lines()
+	print res
+        return res['LineSet']
     except ClientError, e:
         print 'error '+str(e)
-    else:
-        print res 
-        return res['LineSet']
 def describeAddresses(AllocationIdN=None,ProjectIdN=None,MaxResults=None,NextToken=None):
     param = {}
     if AllocationIdN:
@@ -31,17 +30,14 @@ def describeAddresses(AllocationIdN=None,ProjectIdN=None,MaxResults=None,NextTok
     if ProjectIdN:
         param.update(dict(("ProjectId.{}".format(index), item) for index, item in enumerate(ProjectIdN, 1)))        
     try:
-        if param:
-            print param
-            res = eipClient.describe_addresses(**param)
+        print param
+        res = eipClient.describe_addresses(**param)
+        for eip in res['AddressesSet']:
+	    print eip
     except ClientError, e:
         print 'error '+str(e)
-    else:
-        for eip in res['AddressesSet']:
-            print eip
 def allocateAddress(LineId,BandWidth,ChargeType,PurchaseTime=0,ProjectId=0):
     res = None
-
     param = {
             "LineId": LineId,
             "BandWidth": BandWidth,
@@ -51,10 +47,9 @@ def allocateAddress(LineId,BandWidth,ChargeType,PurchaseTime=0,ProjectId=0):
     try:
         if ChargeType == 'Daily':
             res = eipClient.allocate_address(**param)
+	    print res
     except ClientError, e:
         print 'error '+str(e)
-    else:
-        print res 
 def associateAddress(AllocationId,InstanceType,InstanceId,NetworkInterfaceId):
     res = None
     param = {
@@ -65,31 +60,27 @@ def associateAddress(AllocationId,InstanceType,InstanceId,NetworkInterfaceId):
         } 
     try:
         res = eipClient.associate_address(**param)
+	print res
     except ClientError, e:
         print 'error '+str(e)
-    else:
-        print res 
 def disassociateAddress(AllocationId):
     try:
         res = eipClient.disassociate_address(AllocationId=AllocationId)
+	print res
     except ClientError, e:
         print 'error '+str(e)
-    else:
-        print res   
 def releaseAddress(AllocationId):
     try:
         res = eipClient.release_address(AllocationId=AllocationId)
+	print res
     except ClientError, e:
         print 'error '+str(e)
-    else:
-        print res
 def getAccountAllProjectList():
     try:
         res = iamClient.get_account_all_project_list()
+        return res['ListProjectResult']['ProjectList']
     except ClientError, e:
         print 'error '+str(e)
-    else:
-        return res['ListProjectResult']['ProjectList']
 if __name__ == "__main__":
     s = get_session()
     #s.set_credentials(ks_access_key_id, ks_secret_access_key)
@@ -103,11 +94,11 @@ if __name__ == "__main__":
         if line['LineName'] == 'BGP':
             lineId = line['LineId']
     #allocateAddress(lineId,1,'Daily')
-    eips=['d2a6c424-8d32-424f-84d5-f41aef59fd11','ecfae0a6-8d8c-49c9-aa04-20bc2492fc3a']
-    projects=[]
+    #eips=['d2a6c424-8d32-424f-84d5-f41aef59fd11','ecfae0a6-8d8c-49c9-aa04-20bc2492fc3a']
+    projects=[100012,100013]
     #describeAddresses(ProjectIdN=projects)
-    describeAddresses(AllocationIdN=eips,ProjectIdN=projects)
-    print getAccountAllProjectList()
+    describeAddresses(AllocationIdN=[],ProjectIdN=projects)
+    #pprint.pprint(getAccountAllProjectList())
     #associateAddress('2835602d-d12d-4799-bfb0-0331ef1e4edc','Ipfwd','8749bb48-0c63-444a-80ce-0bd9d0d91f34','8fced74a-e232-4916-8023-1febd55fb1a2')
     #disassociateAddress('2835602d-d12d-4799-bfb0-0331ef1e4edc')
     #releaseAddress('2835602d-d12d-4799-bfb0-0331ef1e4edc')
